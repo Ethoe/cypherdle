5a09dd88ee31c64196de334ed4588fb19b546bd3
Ethoe <ryanouttrim@gmail.com>
2024-04-13 12:27:04 -0700

    V2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,139 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="parent">
+    <h1>Cypherdle: Guess the Quote</h1>
+    <h3 v-if="completed">{{ "You Won!" }}</h3>
+    <div class="top">
+      <div
+        class="letter"
+        v-for="letter in statement"
+        :key="letter"
+        @click="handleClick(letter)"
+        :class="{ selected: isSelected(letter), win: completed }"
+      >
+        <CypherLetter
+          :letter="letter"
+          :cypher="cypher"
+          :current="currentSelection"
+          :currentTyped="typedLetter"
+        />
+      </div>
+    </div>
+    <input ref="inputBox" @keydown="handleKeyDown" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import CypherLetter from "./components/CypherLetter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CypherLetter,
+  },
+  data() {
+    return {
+      cypher: 3,
+      statement: "",
+      guessTable: {},
+      currentSelection: "",
+      typedLetter: "",
+      completed: false,
+    };
+  },
+  async created() {
+    try {
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      const response = await axios.get(
+        proxyUrl + "https://zenquotes.io/api/today"
+      );
+      const quote = response.data[0].q; // Extract the quote from the response
+      //var quote = "Hello!, Hello!, Hello!, Hello!, Hello!";
+      const lettersString = quote.toLowerCase().replace(/[^a-zA-Z]/g, "");
+      const uniqueLetters = Array.from(new Set(lettersString.split("")));
+
+      const letterGuesses = {};
+      uniqueLetters.forEach((letter) => {
+        if (!letterGuesses[letter]) {
+          letterGuesses[letter] = "";
+        }
+      });
+
+      this.statement = quote;
+      this.guessTable = letterGuesses;
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  },
+  computed: {
+    isSelected() {
+      return (letter) => {
+        if (this.isLetter(letter) && this.isLetter(this.currentSelection)) {
+          return letter === this.currentSelection;
+        }
+        return false;
+      };
+    },
+  },
+  methods: {
+    handleClick(letter) {
+      this.currentSelection = " ";
+      this.typedLetter = " ";
+      this.currentSelection = letter;
+      this.$refs.inputBox.focus();
+    },
+    isLetter(letter) {
+      return /^[a-zA-Z]$/.test(letter);
+    },
+    handleKeyDown(event) {
+      event.preventDefault();
+      const typedLetter = event.key;
+      if (this.isLetter(typedLetter)) {
+        this.typedLetter = typedLetter;
+        this.guessTable[this.typedLetter] = this.typedLetter;
+        this.checkAllLettersGuessed();
+      }
+    },
+    checkAllLettersGuessed() {
+      const allGuessed = Object.values(this.guessTable).every(
+        (guess) => guess !== ""
+      );
+      if (allGuessed) {
+        console.log("All letters guessed correctly!");
+        this.completed = true;
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+div.parent {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+div.top {
+  display: flex;
+  flex-wrap: wrap;
+  .selected {
+    background-color: #556ae6;
+  }
+
+  .win {
+    background-color: #4caf50;
+  }
+}
+div.letter:hover {
+  background-color: #556ae6;
+}
+
+div.letter {
+  border: 2px solid #494949;
+  border-radius: 8px;
+  width: 80px;
+  height: 100px;
+  margin: 5px;
 }
 </style>
---
+++ src/components/CypherLetter.vue
@@ -0,0 +1,75 @@
+<template>
+  <div v-if="isLetter(letter)">
+    <h1>{{ guess }}</h1>
+    <h3>{{ cypheredLetter }}</h3>
+  </div>
+
+  <div v-else-if="letter === ' '"></div>
+  <div v-else>
+    <h1>{{ letter }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CypherLetter",
+  props: {
+    letter: String,
+    cypher: Number,
+    current: String,
+    currentTyped: String,
+  },
+  data() {
+    return {
+      guess: "_",
+    };
+  },
+  computed: {
+    cypheredLetter() {
+      if (!this.isLetter(this.letter)) {
+        return this.letter;
+      }
+      const lowercaseLetter = this.letter.toLowerCase();
+
+      const currentPosition = lowercaseLetter.charCodeAt(0) - "a".charCodeAt(0);
+      const newPosition = (currentPosition + this.cypher) % 26; // Use modulo to wrap around
+
+      let newLetter = String.fromCharCode("a".charCodeAt(0) + newPosition);
+
+      if (this.letter.toUpperCase() === this.letter) {
+        newLetter = newLetter.toUpperCase();
+      }
+
+      return newLetter;
+    },
+  },
+  methods: {
+    isLetter(letter) {
+      return /^[a-zA-Z]$/.test(letter);
+    },
+    currentGuess() {
+      if (
+        this.isLetter(this.currentTyped) &&
+        this.current.toLowerCase() == this.letter.toLowerCase()
+      ) {
+        if (this.letter.toUpperCase() === this.letter) {
+          this.guess = this.currentTyped.toUpperCase();
+        } else {
+          this.guess = this.currentTyped.toLowerCase();
+        }
+      }
+    },
+  },
+  watch: {
+    currentTyped() {
+      this.currentGuess();
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
